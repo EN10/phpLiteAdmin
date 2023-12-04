966190fe3611e50ab1811db178633418fe25692f
eniompw <71254710+eniompw@users.noreply.github.com>
2023-12-04 08:25:33 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+# download code files
+git clone https://github.com/eniompw/phpLiteAdmin
+cd phpLiteAdmin
+# move linked php and css files
+mkdir -p /opt/cs50/phpliteadmin/
+mv ./share/ /opt/cs50/phpliteadmin/
+# make phpliteadmin executable
+chmod +x phpliteadmin.py
+# add to path, allows running by name only
+sudo mv phpliteadmin.py /usr/local/bin
+# remove unneeded code
+rm ../phpLiteAdmin -fr
+cd ..
+# install requirements
+pip install termcolor
+sudo apt update
+sudo apt install phpliteadmin -y
