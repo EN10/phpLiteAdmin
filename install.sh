3b3450cb1105cdb461fdedecbafc41e28e53f824
eniompw <71254710+eniompw@users.noreply.github.com>
2023-12-04 10:54:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ git clone https://github.com/eniompw/phpLiteAdmin
 cd phpLiteAdmin
 # move linked php and css files
 sudo mkdir -p /opt/cs50/phpliteadmin/
-mv ./share/ /opt/cs50/phpliteadmin/
+sudo mv ./share/ /opt/cs50/phpliteadmin/
 # make phpliteadmin executable
 chmod +x phpliteadmin.py
 # add to path, allows running by name only
