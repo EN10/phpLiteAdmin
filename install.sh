78a4505ac67b48b47af9177590aeb0963af0f68e
eniompw <71254710+eniompw@users.noreply.github.com>
2024-04-29 19:51:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,4 +16,4 @@ pip install termcolor
 sudo apt update
 sudo apt install phpliteadmin -y
 sudo apt install python-is-python3 -y
-alias pla=phpliteadmin.py
+sudo ln -s /usr/local/bin/phpliteadmin.py /usr/local/bin/pla
